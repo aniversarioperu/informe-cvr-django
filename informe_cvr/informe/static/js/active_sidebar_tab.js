01d824ff9929d2d034c71d5b80d9f681ab30f140
aniversarioperu <yoni@aniversarioperu.me>
2015-10-03 20:13:35 +0300

    fixed highlighting of sidebar when using anchor

--- informe_cvr/informe/static/js/active_sidebar_tab.js
+++ informe_cvr/informe/static/js/active_sidebar_tab.js
@@ -1,6 +1,7 @@
 <!-- this highlights current tab in navegation bar -->
 $(document).ready(function() {
     var pathname = window.location.pathname;
+    pathname = '/' + pathname.split('/')[1] + '/'
 
     $(".nav a").each(function(index, value) {
         if (pathname == $(this).attr('href'))
