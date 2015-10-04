123e15c1973dd3b6d6bc3be15a23986126cb4cf1
aniversarioperu <yoni@aniversarioperu.me>
2015-10-04 20:59:21 +0300

    start tests

--- informe_cvr/informe/tests.py
+++ informe_cvr/informe/tests.py
@@ -1,12 +1,28 @@
+import os
+
 from django.test import TestCase
 from django.test.client import Client
+from django.conf import settings
+
+from informe.models import Entry
+
+
+TEST_FILE = os.path.join(settings.BASE_DIR, '..', '..', 'source_files',
+                         'prefacio.md')
 
 
 class TestViews(TestCase):
     def setUp(self):
         self.c = Client()
+        with open(TEST_FILE, 'r') as handle:
+            prefacio = handle.read()
+            e = Entry(title='PREFACIO', body=prefacio, slug='prefacio')
+            e.save()
 
     def test_index(self):
         result = self.c.get('/')
         self.assertEqual(200, result.status_code)
 
+    def test_search(self):
+        result = self.c.get('/search/?q=historia', follow=True)
+        self.assertEqual(200, result.status_code)
